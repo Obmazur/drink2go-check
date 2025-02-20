7f9be5604b1f68940721b073e574507908afb64a
Alexander Mazur <iamdinarium@gmail.com>
2025-02-20 22:03:47 +0300

    Стилизует шапку для мобильной версии

---
+++ source/scripts/main.js
@@ -0,0 +1,9 @@
+const navMainElement = document.querySelector('.main-nav');
+const navToggleElement = document.querySelector('.main-nav__toggle');
+
+const toggleMenu = (button) => {
+  button.classList.toggle('main-nav--is-closed');
+  button.classList.toggle('main-nav--is-open');
+};
+
+navToggleElement.addEventListener('click', toggleMenu(navMainElement));
